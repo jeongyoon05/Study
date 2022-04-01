c2c5c944edfa59abc6cd52f64ee39cb9f59c193e
Jeong Yoon <dlwjddbs14@naver.com>
2022-04-01 22:40:40 +0900

    🌟 :: 프로젝트 세팅

---
+++ Mock/src/main/java/com/example/mocktestproject/MockTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mocktestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockTestProjectApplication.class, args);
+    }
+
+}
---
+++ Mock/src/test/java/com/example/mocktestproject/MockTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mocktestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
