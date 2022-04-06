06e3dd511febb5694178071dbd68d52a742743bf
Jeong Yoon <dlwjddbs14@naver.com>
2022-04-06 22:39:33 +0900

    🌟 :: 프로젝트 세팅

---
+++ querydsl/src/main/java/com/example/querydsltest/QuerydslTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslTestApplication.class, args);
+    }
+
+}
---
+++ querydsl/src/test/java/com/example/querydsltest/QuerydslTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydsltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
