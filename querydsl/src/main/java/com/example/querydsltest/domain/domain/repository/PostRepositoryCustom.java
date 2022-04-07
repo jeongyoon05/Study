a2f3b093963fa1b8bdc5701a78e0f9d90f56ffbd
Jeong Yoon <dlwjddbs14@naver.com>
2022-04-07 23:11:40 +0900

    🌨 :: querydsl 설정

--- querydsl/src/main/java/com/example/querydsltest/domain/domain/repository/PostRepositoryCustom.java
+++ querydsl/src/main/java/com/example/querydsltest/domain/domain/repository/PostRepositoryCustom.java
@@ -1,6 +1,10 @@
 package com.example.querydsltest.domain.domain.repository;
 
+import com.example.querydsltest.domain.api.dto.response.QueryPostResponse;
+import org.springframework.data.domain.Pageable;
+
 import java.util.List;
 
 public interface PostRepositoryCustom {
+    List<QueryPostResponse> searchPostList(String keyword, Pageable pageable);
 }
--- querydsl/src/main/java/com/example/querydsltest/domain/domain/repository/PostRepositoryCustomImpl.java
+++ querydsl/src/main/java/com/example/querydsltest/domain/domain/repository/PostRepositoryCustomImpl.java
@@ -1,12 +1,31 @@
 package com.example.querydsltest.domain.domain.repository;
 
+import com.example.querydsltest.domain.api.dto.response.QQueryPostResponse;
+import com.example.querydsltest.domain.api.dto.response.QueryPostResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+
+import static com.example.querydsltest.domain.domain.QPost.post;
 
 @RequiredArgsConstructor
 public class PostRepositoryCustomImpl implements PostRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<QueryPostResponse> searchPostList(String keyword, Pageable pageable) {
+        return queryFactory
+                .select(new QQueryPostResponse(
+                        post.title,
+                        post.content
+                ))
+                .from(post)
+                .where(post.title.contains(keyword))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
 }
