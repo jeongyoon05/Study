f3a5e1f6d4e00ff09dec669370b9ea69363bc733
Jeong Yoon <dlwjddbs14@naver.com>
2022-04-25 10:52:34 +0900

    🌱 :: ex

---
+++ data-structure/src/simple/character/CharacterEx.java
@@ -0,0 +1,12 @@
+package simple.character;
+
+public class CharacterEx {
+    public static void main(String[] args) {
+        char c = ' '; //비어있음
+
+        System.out.println("char value : " + c);
+        c = 'E';
+        System.out.println(c);
+
+    }
+}
